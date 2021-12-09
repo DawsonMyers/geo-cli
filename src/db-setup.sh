3f816b6300b0f1a85f067eb04cfbd5c046719f9c
dawsonmyers <dawsonmyers@geotab.com>
2021-12-09 16:07:46 -0500

    Remove old file

--- src/db-setup.sh
+++
@@ -1,8 +0,0 @@
-# pgadmin > set server properties to use user geotabuser > click on db under server > enter vircom43 as password
-
-# dotnet CheckmateServer.dll CreateDatabase postgres companyName=geotabdemo administratorUser=<your email address> administratorPassword=<choose a password> sqluser=geotabuser sqlpassword=vircom43
-
-# Nav to MyGeotab/Checkmate/bin/Debug/netcoreapp3.1
-cd repos/MyGeotab/Checkmate/bin/Debug/netcoreapp3.1/
-
-dotnet CheckmateServer.dll CreateDatabase postgres companyName=geotabdemo administratorUser=dawsonmyers@geotab.com administratorPassword=password sqluser=geotabuser sqlpassword=vircom43
