2681b9de63e22fd259885b941c841b0a837eb2ca
Dawson Myers <dawsonmyers@geotab.com>
2022-11-08 12:49:03 -0500

    Clean up config file utils

--- src/utils/config-file-utils.sh
+++ src/utils/config-file-utils.sh
@@ -1,34 +1,42 @@
-sed_escape() {
-  sed -e 's/[]\/$*.^[]/\\&/g'
+cfg_key_lookup_escape() {
+    sed -e 's/[]\/$*.^[]/\\&/g'
 }
 
-sed_value_escape() {
-  sed ':a;N;$!ba;s/\n/__n__/g'
+cfg_value_escape() {
+    sed ':a;N;$!ba;s/\n/__n__/g'
 }
 
-sed_value_unescape() {
-  sed 's/__n__/\n/g'
+cfg_value_unescape() {
+    sed 's/__n__/\n/g'
 }
 
 cfg_write() { # path, key, value
-  if [[ $2 = 'GEO_REPO_DIR' && $3 = '' ]]; then
-    echo "Warning: config-file-utils:cfg_write: preventing '' from being writen to GEO_REPO_DIR"
-    return 1
-  fi
-  local key=$(echo "$2" | sed_escape)
-  local value=$(echo "$3" | sed_value_escape)
-  cfg_delete "$1" "$key"
-  echo "$key=$value" >> "$1"
+    if [[ $2 = 'GEO_REPO_DIR' && $3 = '' ]]; then
+        echo "Warning: config-file-utils:cfg_write: preventing '' from being writen to GEO_REPO_DIR"
+        return 1
+    fi
+    local file="$1"
+    # Don't escape the key when writing to file. We only want to escape it when looking for it in a file.
+    local key="$2"
+    local value=$(echo "$3" | cfg_value_escape)
+    cfg_delete "$file" "$key"
+    echo "$key=$value" >> "$file"
 }
 
 cfg_read() { # path, key -> value
-  test -f "$1" && grep "^$(echo "$2" | sed_escape)=" "$1" | sed "s/^$(echo "$2" | sed_escape)=//" | tail -1 | sed_value_unescape
+    local file="$1"
+    local key=$(echo "$2" | cfg_key_lookup_escape)
+    test -f "$file" && grep "^$key=" "$file" | sed "s/^$key=//" | tail -1 | cfg_value_unescape
 }
 
 cfg_delete() { # path, key
-  test -f "$1" && sed -i "/^$(echo $2 | sed_escape)=.*$/d" "$1"
+    local file="$1"
+    local key=$(echo "$2" | cfg_key_lookup_escape)
+    test -f "$file" && sed -i "/^$key="'.*$/d' "$file"
 }
 
 cfg_haskey() { # path, key
-  test -f "$1" && grep "^$(echo "$2" | sed_escape)=" "$1" > /dev/null
+    local file="$1"
+    local key=$(echo "$2" | cfg_key_lookup_escape)
+    test -f "$file" && grep "^$key=" "$file" >/dev/null
 }
