1b836ef619ae7a043118ddb427829f1ba35848a8
Lokesh Gupta <lokeshgupta@geotab.com>
2022-12-07 17:13:16 -0500

    open myg in diff terminal window

--- src/utils/cli-handlers.sh
+++ src/utils/cli-handlers.sh
@@ -4790,7 +4790,7 @@ geo_myg() {
 
 _geo_run_myg_gw(){
 
-local db_name=
+# local db_name=
 
 # prompt_for_db_name() {
 #         while [[ -z $db_name ]]; do
@@ -4811,16 +4811,16 @@ local db_name=
 
 # geo_db start -d $db_name -p
 
-# geo_db psql -d $db_name -q "INSERT INTO public.vehicle (iid, sserialno, ihardwareid, sdescription, iproductid, svin, slicenseplate, slicensestate, scomments, isecstodownload, dtignoredownload, iworktimesheaderid, stimezoneid, sparameters, ienginetypeid, irowversion, senginevin, dtactivefrom, dtactiveto) VALUES (1, 'GV0100000001', 1, 'DeviceSimulator', 81, '', '', '', '', 86400, '1986-01-01 00:00:00', 1, 'America/New_York', '{"major": 14, "minor": 20, "autoHos": "AUTO", "channel": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "version": "0000000000000004", "activeTo": "2050-01-01T00:00:00.000Z", "rpmValue": 3500, "pinDevice": true, "activeFrom": "2020-12-03T19:38:13.727Z", "speedingOn": 100.0, "gpsOffDelay": 0, "idleMinutes": 3, "speedingOff": 90.0, "channelCount": 1, "licensePlate": "", "licenseState": "", "disableBuzzer": false, "isAuxInverted": [false, false, false, false, false, false, false, false], "ensureHotStart": false, "goTalkLanguage": "English", "immobilizeUnit": false, "odometerFactor": 1.0, "odometerOffset": 0.0, "auxWarningSpeed": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "enableBeepOnRpm": false, "frequencyOffset": 1, "isAuxIgnTrigger": [false, false, false, false], "customParameters": [], "enableAuxWarning": [false, false, false, false, false, false, false, false], "enableBeepOnIdle": false, "engineHourOffset": 0, "fuelTankCapacity": 0.0, "immobilizeArming": 30, "isSpeedIndicator": false, "minAccidentSpeed": 4.0, "parameterVersion": 1, "isAidedGpsEnabled": false, "isReverseDetectOn": false, "enableSpeedWarning": false, "rfParameterVersion": 0, "disableSleeperBerth": false, "enableMustReprogram": false, "seatbeltWarningSpeed": 10.0, "maxSecondsBetweenLogs": 200.0, "isIoxConnectionEnabled": true, "isRfUploadOnWhenMoving": false, "brakingWarningThreshold": -34, "isActiveTrackingEnabled": false, "parameterVersionOnDevice": 0, "corneringWarningThreshold": 26, "isDriverSeatbeltWarningOn": false, "enableControlExternalRelay": false, "externalDeviceShutDownDelay": 0, "accelerationWarningThreshold": 24, "enableBeepOnDangerousDriving": false, "isPassengerSeatbeltWarningOn": false, "accelerometerThresholdWarningFactor": 0, "isExternalDevicePowerControlSupported": true}', 9999, 5, '?', '2020-12-03T19:38:13.727Z', '2050-01-01T00:00:00.000Z')"
+# # geo_db psql -d $db_name -q "INSERT INTO public.vehicle (iid, sserialno, ihardwareid, sdescription, iproductid, svin, slicenseplate, slicensestate, scomments, isecstodownload, dtignoredownload, iworktimesheaderid, stimezoneid, sparameters, ienginetypeid, irowversion, senginevin, dtactivefrom, dtactiveto) VALUES (1, 'GV0100000001', 1, 'DeviceSimulator', 81, '', '', '', '', 86400, '1986-01-01 00:00:00', 1, 'America/New_York', '{"major": 14, "minor": 20, "autoHos": "AUTO", "channel": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "version": "0000000000000004", "activeTo": "2050-01-01T00:00:00.000Z", "rpmValue": 3500, "pinDevice": true, "activeFrom": "2020-12-03T19:38:13.727Z", "speedingOn": 100.0, "gpsOffDelay": 0, "idleMinutes": 3, "speedingOff": 90.0, "channelCount": 1, "licensePlate": "", "licenseState": "", "disableBuzzer": false, "isAuxInverted": [false, false, false, false, false, false, false, false], "ensureHotStart": false, "goTalkLanguage": "English", "immobilizeUnit": false, "odometerFactor": 1.0, "odometerOffset": 0.0, "auxWarningSpeed": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "enableBeepOnRpm": false, "frequencyOffset": 1, "isAuxIgnTrigger": [false, false, false, false], "customParameters": [], "enableAuxWarning": [false, false, false, false, false, false, false, false], "enableBeepOnIdle": false, "engineHourOffset": 0, "fuelTankCapacity": 0.0, "immobilizeArming": 30, "isSpeedIndicator": false, "minAccidentSpeed": 4.0, "parameterVersion": 1, "isAidedGpsEnabled": false, "isReverseDetectOn": false, "enableSpeedWarning": false, "rfParameterVersion": 0, "disableSleeperBerth": false, "enableMustReprogram": false, "seatbeltWarningSpeed": 10.0, "maxSecondsBetweenLogs": 200.0, "isIoxConnectionEnabled": true, "isRfUploadOnWhenMoving": false, "brakingWarningThreshold": -34, "isActiveTrackingEnabled": false, "parameterVersionOnDevice": 0, "corneringWarningThreshold": 26, "isDriverSeatbeltWarningOn": false, "enableControlExternalRelay": false, "externalDeviceShutDownDelay": 0, "accelerationWarningThreshold": 24, "enableBeepOnDangerousDriving": false, "isPassengerSeatbeltWarningOn": false, "accelerometerThresholdWarningFactor": 0, "isExternalDevicePowerControlSupported": true}', 9999, 5, '?', '2020-12-03T19:38:13.727Z', '2050-01-01T00:00:00.000Z')"
 
-# geo_db psql -d $db_name -q "INSERT INTO public.nodevehicle (inodeid, ivehicleid, gid) VALUES (9998, 1, '52bc3790-01b4-47ef-9853-255589e30997')"
+# # geo_db psql -d $db_name -q "INSERT INTO public.nodevehicle (inodeid, ivehicleid, gid) VALUES (9998, 1, '52bc3790-01b4-47ef-9853-255589e30997')"
 
 # local server_config="$HOME/GEOTAB/Checkmate/server.config"
 # local store_config="$HOME/GEOTAB/Checkmate/storeforward.config"
 
 # xmlstarlet ed --inplace -u "//WebServerSettings/WebPort" -v 10000 -u "//WebServerSettings/WebSSLPort" -v 10001 "$server_config"
 
-# xmlstarlet ed --inplace -u "//WebServerSettings/ServiceSettings/ServerSettings/LiveSettings/ServerAddress" -v 127.0.0.1 -u "//WebServerSettings/ServiceSettings/ServerSettings/LiveSettings/ServerPort" -v 3982 "$server_config"
+# xmlstarlet ed --inplace -u "//WebServerSettings/ServiceSettings/ServerSettings[DatabaseSettingsInternal[ConnectedSqlServerDatabase='$db_name']]/LiveSettings/ServerAddress" -v localhost -u "//WebServerSettings/ServiceSettings/ServerSettings[DatabaseSettingsInternal[ConnectedSqlServerDatabase='$db_name']]/LiveSettings/ServerPort" -v 3982 "$server_config"
 
 # xmlstarlet ed --inplace -u "//StoreForwardSettings/Type" -v Developer -u "//WebServerSettings/WebSSLPort" -v 10001 "$store_config"
 
@@ -4837,15 +4837,21 @@ local db_name=
 
 # xmlstarlet ed --inplace -s "//StoreForwardSettings/ClientListenerEndPoints/IPEndPoint" -t elem -n Port -v 3982 "$store_config"
 
-# # geo_myg build
+# geo_myg build
 
 # local dev_repo=$(geo_get DEV_REPO_DIR)
 
 # sudo cp $dev_repo/gitlab-ci/dockerfiles/MygTestContainer/geotabcommoncertroot.crt /usr/local/share/ca-certificates/root.crt
 
-# sudo update-ca-certificates
+# # sudo update-ca-certificates
+# echo -e '#!/bin/sh\n echo hello' > abc.sh
+# chmod +x abc.sh
+# gnome-terminal -- ./abc.sh
 
-# geo_gw start
+gnome-terminal -e "bash -c '$GEO_CLI_SRC_DIR/geo-cli.sh myg start; sleep 3'"
+
+#geo_gw start
+
 
 # #geo_myg start
 
