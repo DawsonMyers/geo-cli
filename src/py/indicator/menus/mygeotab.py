5f81085e43301a6cee064ba16c4afc77552b331d
Joy Fang <joyfang@geotab.com>
2023-01-05 12:36:58 -0500

    Add UI for option stop myg and gw

--- src/py/indicator/menus/mygeotab.py
+++ src/py/indicator/menus/mygeotab.py
@@ -43,7 +43,9 @@ class MyGeotabMenuItem(Gtk.MenuItem):
         start_item = Gtk.MenuItem(label='Start')
         start_item.connect('activate', lambda _: self.start_or_restart_myg('start'))
         stop_item = Gtk.MenuItem(label='Stop')
-        stop_item.connect('activate', lambda _: geo.run('myg stop'))
+        stop_item.connect('activate', lambda _: geo.run('myg stop'))        
+        stop_myg_gw_item = Gtk.MenuItem(label='Stop MyG and GW')
+        stop_myg_gw_item.connect('activate', lambda _: geo.run('myg stop-myg-gw'))
         restart_item = Gtk.MenuItem(label='Restart')
         restart_item.connect('activate', lambda _: self.start_or_restart_myg('restart'))
         build_item = Gtk.MenuItem(label='Build')
@@ -64,6 +66,7 @@ class MyGeotabMenuItem(Gtk.MenuItem):
         submenu.append(clean_item)
         submenu.append(run_with_gateway_item)
         submenu.append(stop_item)
+        submenu.append(stop_myg_gw_item)
         submenu.append(restart_item)
         submenu.append(browser_item)
         submenu.append(api_item)
@@ -78,7 +81,8 @@ class MyGeotabMenuItem(Gtk.MenuItem):
             "restart": restart_item,
             "browser": browser_item,
             "api": api_item,
-            "run_with_gateway": run_with_gateway_item
+            "run_with_gateway": run_with_gateway_item,
+            "stop_myg_gw": stop_myg_gw_item
         }
         
         self.running_items = {
@@ -100,8 +104,9 @@ class MyGeotabMenuItem(Gtk.MenuItem):
         geo.run_in_terminal(f'myg {cmd}', title=title)
 
     def monitor(self):
-        is_running = geo.run('myg is-running')
-        if is_running:
+        is_myg_running = geo.run('myg is-running')
+        is_gw_running = 
+        if is_myg_running:
             self.app.icon_manager.set_myg_running(True)
             self.show_running_items()
         else:
